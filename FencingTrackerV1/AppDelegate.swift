2ba85cb18796c47646a45644c6fb912ff6a3a9ef
Earlight <earlightny@gmail.com>
2020-02-08 20:05:33 -0500

    no message

--- FencingTrackerV1/AppDelegate.swift
+++ FencingTrackerV1/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Earlight on 1/18/20.
 //  Copyright © 2020 Richard. All rights reserved.
 //
-
+import LeanCloud
 import UIKit
 
 @UIApplicationMain
@@ -13,7 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        LCApplication.logLevel = .all
+        do {
+            try LCApplication.default.set(
+                id: "0xztkWjU0QbDBaPlvko0lIR8-MdYXbMMI",
+                key: "v5gnPEM4xOyUQPNuJM9DrnAE",
+                serverURL: "https://xxx.example.com")
+        } catch {
+            print(error)
+        }
         // Override point for customization after application launch.
         return true
     }
--- FencingTrackerV1/LoginViewController.swift
+++ FencingTrackerV1/LoginViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Earlight on 1/18/20.
 //  Copyright © 2020 Richard. All rights reserved.
 //
-
+import LeanCloud
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -15,6 +15,17 @@ class LoginViewController: UIViewController {
     var name: String = ""
     
     override func viewDidLoad() {
+        do {
+            let testObject = LCObject(className: "Account")
+            try testObject.set("test", value: "Hello world!")
+            let result = testObject.save()
+            if let error = result.error {
+                print("do1234......\(error)")
+            }
+        } catch {
+            print("catch1234......\(error)")
+        }
+        
         super.viewDidLoad()
         name = "slime"
     }
